d94e9ffbe393287c61ffa09320a369e49a4dcee9
BlakeDarrow <71980368+BlakeDarrow@users.noreply.github.com>
2021-04-02 22:00:56 -0500

    v0.12.2 Test

--- darrow_toolkit/__init__.py
+++ darrow_toolkit/__init__.py
@@ -29,7 +29,7 @@
 bl_info = {
     "name": "Darrow Toolkit",
     "author": "Blake Darrow",
-    "version": (0, 12, 1),
+    "version": (0, 12, 2),
     "blender": (2, 90, 0),
     "location": "View3D > Sidebar > Darrow Toolkit",
     "description": "Toolkit for efficient FBX exporting",
